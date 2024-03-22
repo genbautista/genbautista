bf1e8ac6cea9059fd9cbd667ef554dd3756a5e81
genbautista <160640511+genbautista@users.noreply.github.com>
2024-03-22 11:27:35 -0400

    Update homepage.js

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -27,18 +27,17 @@ const HomePage = () => {
                    <span className="line"></span>
                    <span className="line"></span>
                    <span className="line"></span>
-                   <span className="line"></span>
                </div>
                <div className="nav-menu hide">
                    <a href="#" className="button">Home</a>
                    <a href="#" className="button">Progress Tracker</a>
-                   <button onClick={handleGoalSettingClick} className="button">Goal Setting</button> {/* Use a button element and handle its click event */}
+                   <button onClick={handleGoalSettingClick} className="button">Goal Setting/Report Spendings</button> {/* Use a button element and handle its click event */}
                    <a href="#" className="button">Budget Management</a>
-                   <a href="#" className="button">Report Spending Habits</a>
                </div>
            </nav>
            <main>
                <h2>Welcome to my home page! Do you want to keep track of your shopping habits? You've arrived at the right place!</h2>
+               <p>In order to begin please click on the Goal Setting/Report button to specify on your spendings.</p>
                {/* Conditionally render the ShoppingHabitsForm component based on the state */}
                {showForm && <ShoppingHabitsForm />}
            </main>
