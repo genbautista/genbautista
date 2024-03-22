0dda71acc8608f18b109e89f70b5d567c9d653a8
genbautista <160640511+genbautista@users.noreply.github.com>
2024-03-22 11:26:51 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/homepage';
-import Calendar from './Calender';
+import Calendar from './Calender'; 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
-     <div>
-       <Calendar />
-     </div>
-     <footer>
-       <p>Made in 2024 by Genesis Bautista</p>
-     </footer>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePageWithCalendar />} />
+        </Routes>
+      </Router>
+      <footer>
+        <p>Made in 2024 by Genesis Bautista</p>
+      </footer>
+    </div>
+  );
+}
 
+function HomePageWithCalendar() {
+  return (
+    <div>
+      <HomePage />
+      <Calendar />
     </div>
   );
 }
