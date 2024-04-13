f0a8d17ed07736f567995d03d2a8a1e6d3db876b
genbautista <160640511+genbautista@users.noreply.github.com>
2024-04-13 15:50:51 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/homepage';
 import Calendar from './Calender'; 
 import './App.css';
+import { ProgressBar } from './components/progresstracker';
 
 function App() {
+  const [progress, setProgress] = useState(0);
+
+  const updateProgress = (newProgress) => {
+    setProgress(newProgress);
+  };
+
+  console.log('Current progress:', progress);
+
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/" element={<HomePageWithCalendar />} />
+          <Route path="/progress" element={<ProgressBar updateProgress={updateProgress} />} />
         </Routes>
       </Router>
       <footer>
