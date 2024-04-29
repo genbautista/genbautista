d18898320e3c0f7867131c50157a18a646a10950
genbautista <160640511+genbautista@users.noreply.github.com>
2024-04-29 15:42:58 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<HomePageWithCalendar />} />
-          {/* Use ProgressTracker component instead of ProgressBar */}
+          {}
           <Route path="/progress" element={<ProgressTracker />} />
         </Routes>
       </Router>
